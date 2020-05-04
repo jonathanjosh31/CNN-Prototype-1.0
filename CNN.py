9fa8ef65fc288b2404743e07810a2c4b86c72fed
Jonathan Joseph <44317651+jonathanjosh31@users.noreply.github.com>
2020-05-04 17:20:27 +0530

    Updated the CNN.py by initialising and adding features
    Initialised the CNN  by using the classifier
    Added the Convolution Layer
    Added the Max Pooling  Layer

--- CNN.py
+++ CNN.py
@@ -15,4 +15,19 @@ from tensorflow.keras.layers import MaxPooling2D
 from tensorflow.keras.layers import Flatten
 from tensorflow.keras.layers import Dense
 
+#Initialisation of the CNN by making a classifier which is a Sequential object
+cnn_classifier = Sequential()
+
+#Layer 1 - Convolutional Layer
+cnn_classifier.add(Convolution2D(32,3,3,input_shape = (64,64,3),activation = 'relu'))    #Here we added a new method to the classifier object which is of Convolution2d
+
+#Layer 2 - Pooling Layer
+'''
+Mainly reduces the size of our pooling Map
+We use Max Pooling Here.For More Pooling details please refer the pdfs attached.
+All those convolution layers size will be reduced by half.
+'''
+
+cnn_classifier.add(MaxPooling2D(pool_size = (2,2)))
+
 
